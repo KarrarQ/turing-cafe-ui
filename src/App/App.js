e4b4de58381898f80f3302380fad7ddd5de1d5c1
Karrar Qasim <Karawr1996@gmail.com>
2023-02-21 09:35:21 -0700

    Added fetch for all reservations upon app mounting

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
+// import { reservations } from './data';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: [],
+      error: false
+    }
+  }
+
+  componentDidMount = () => {
+    fetch(`http://localhost:3001/api/v1/reservations`)
+      .then(response => {
+        if (response.ok) {
+          this.setState({ error: false})
+          return response.json()
+        } else {
+          throw Error(response.statusText)
+        }
+      })
+      .then(data => this.setState({ reservations: data }))
+      .catch((error) => {
+        this.setState({ error: true })
+      }
+    )
+  }
+
   render() {
     return (
       <div className="App">
